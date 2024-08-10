8e2d51838c1f9d8fa75694b3181308be05e797d7
shinder <shinder@MAVPUN0522030.mavericsystems.lan>
2024-08-10 12:52:59 +0530

    RushiMavenFirstCommit

---
+++ rushi-maven/src/main/java/com/rushi/org/rushi_maven/App.java
@@ -0,0 +1,13 @@
+package com.rushi.org.rushi_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
